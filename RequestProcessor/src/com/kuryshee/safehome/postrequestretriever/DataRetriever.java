38674948ef1016a5214eefed2ed0d18a365dd396
katrikken <kurysheva.katerina@gmail.com>
2017-06-10 15:29:33 +0200

    Comments added

--- RequestProcessor/src/com/kuryshee/safehome/postrequestretriever/DataRetriever.java
+++ RequestProcessor/src/com/kuryshee/safehome/postrequestretriever/DataRetriever.java
@@ -14,9 +14,21 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.Part;
 
+/**
+ * This class implements retrieval of data from POST multi-part requests. 
+ * @author Ekaterina Kurysheva
+ */
 public class DataRetriever {
+	
 	private static final String DEFAULT_ENCODING = "UTF-8";
 	
+	/**
+	 * This method retrieves data of a media file form the POST multi-part request.
+	 * @param request is a POST request.
+	 * @param name is a name of the file part.
+	 * @param path is a directory where the file should be stored in.
+	 * @return true if writing data file to the path was successfully finished.
+	 */
 	public Boolean saveFilePart(HttpServletRequest request, String name, String path){
 		try{
 			Part filePart = request.getPart(name); 
@@ -24,9 +36,9 @@ public class DataRetriever {
 			File file = new File(path);
 			byte[] buffer = new byte[fileContent.available()];	
 		    fileContent.read(buffer);
-		    OutputStream outStream = new FileOutputStream(file);
-		    outStream.write(buffer);
-		    
+		    try(OutputStream outStream = new FileOutputStream(file)){
+		    	outStream.write(buffer);
+		    }
 		    return true;
 		}
 		catch(IOException | ServletException e){
@@ -35,24 +47,32 @@ public class DataRetriever {
 		return false;
 	}
 	
+	/**
+	 * This method retrieves text data from the multi-part POST request.
+	 * @param request is a multi-part POST request.
+	 * @param name is the name of the data value.
+	 * @return data as a string. In case no data were found returns empty string.
+	 */
 	public String getTextPart(HttpServletRequest request, String name){
 		try{
 			Part textPart = request.getPart(name);
-
-			String encoding = request.getCharacterEncoding();
-			if(encoding == null){
-				encoding = DEFAULT_ENCODING;
-			}
 			
-			BufferedReader reader = new BufferedReader(
-					new InputStreamReader(textPart.getInputStream(), encoding));
-			StringBuilder value = new StringBuilder();
-			char[] buffer = new char[1024];
-		    for (int length = 0; (length = reader.read(buffer)) > 0;) {
-		    	value.append(buffer, 0, length);
-		    }
-		    
-		    return value.toString();
+			if(textPart != null){
+				String encoding = request.getCharacterEncoding();
+				if(encoding == null){
+					encoding = DEFAULT_ENCODING;
+				}
+				
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(textPart.getInputStream(), encoding));
+				StringBuilder value = new StringBuilder();
+				char[] buffer = new char[1024];
+			    for (int length = 0; (length = reader.read(buffer)) > 0;) {
+			    	value.append(buffer, 0, length);
+			    }
+			    
+			    return value.toString();
+			}
 		}
 		catch(IOException | ServletException e){
 			Logger.getLogger("Text retrieving").log(Level.SEVERE, "Exception while retrieving data", e);
