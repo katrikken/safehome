4e942f91af7b9ea54b5cd021784b4fb01d343544
katrikken <kurysheva.katerina@gmail.com>
2017-06-12 23:53:22 +0200

    Database interface

---
+++ RequestProcessor/src/com/kuryshee/safehome/database/DatabaseAccessInterface.java
@@ -0,0 +1,33 @@
+package com.kuryshee.safehome.database;
+
+import java.util.Map;
+
+/**
+ * This interface is an abstraction of interactions with the database.
+ * @author Ekaterina Kurysheva.
+ *
+ */
+public interface DatabaseAccessInterface {
+	
+	/**
+	 * This method performs connection to the database.
+	 * @param url is the link to the database.
+	 * @param properties is a map where keys and values correspond to tag/value pairs for the connection.
+	 * @return true if connection succeeded.
+	 */
+	public Boolean connect(String url, Map<String,String> properties);
+	
+	/**
+	 * This method performs SQL INSERT command.
+	 * @param table is a name of the table.
+	 * @param values is a map, where keys are names of table rows and values are values to be inserted.
+	 * @return true if insertion was successful.
+	 */
+	public Boolean insert(String table, Map<String,String> values);
+	
+	/**
+	 * This method closes the connection to the database.
+	 * @return true if the connection was successfully closed.
+	 */
+	public Boolean close();
+}
