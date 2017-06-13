0b5a32d85db955627497fc2e87d1762c403245b4
katrikken <kurysheva.katerina@gmail.com>
2017-06-13 20:26:25 +0200

    Comments added

--- SafeHome/src/com/kuryshee/safehome/server/SafeHomeServer.java
+++ SafeHome/src/com/kuryshee/safehome/server/SafeHomeServer.java
@@ -1,10 +1,6 @@
 package com.kuryshee.safehome.server;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ConcurrentMap;
@@ -17,47 +13,88 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 
 import com.kuryshee.safehome.requestprocessorinterface.RequestProcessor;
 
+/**
+ * This class extends {@link HttpServlet} class and overrides its doGet and doPost methods.
+ * @author Ekaterina Kurysheva
+ */
 @WebServlet(loadOnStartup = 1, urlPatterns = {"/SafeHomeServer", "/SafeHomeServer/rpi/*", "/SafeHomeServer/app/*"})
 @MultipartConfig
-/**
- * This class extends HttpServlet and overrides doGet and doPost methods.
- */
 public class SafeHomeServer extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 	
+	/**
+	 * URL address on which this servlet accepts requests form Raspberry Pi.
+	 */
 	private static final String servletPathRPi = "/SafeHome/SafeHomeServer/rpi";
+	
+	/**
+	 * URL address on which this servlet accepts requests form the client application.
+	 */
 	private static final String servletPathApp = "/SafeHome/SafeHomeServer/app";
 	
-	private static final String STD_CHARSET = "charset=UTF-8";
-	
-	public static final String REQ_CHECKTASK = "/checktask";
-	public static final String REQ_PHOTOTAKEN = "/photo";  
-	public static final String REQ_RFIDSWITCH = "/rfid";  
-    public static final String REQ_MOTIONDETECTED = "/motiondetected";
-	
-	public static final String COMMAND_GETSTATE = "/getstate";
-	public static final String COMMAND_SWITCHOFF = "/switchoff";
-	public static final String COMMAND_SWITCHON = "/switchon";
-	public static final String COMMAND_TAKEPHOTO = "/takephoto";
-	
-	public static final String UPLOAD_PHOTO = "/upload"; 
-	
-	public static final String NO_ANSWER = "no answer";
-	
-	public static final String OK_ANSWER = "ok";
-	public static final String ERROR_ANSWER = "error";
-	
-	public static final String RPI_PARAM = "rpi";
-	public static final String ANSWR_PARAM = "answer";
 	/**
-     * The constant for the POST request parameter containing the name associated with used RFID token.
+     * The constant for GET request to check up with the server.
      */
-    public static final String RFID_PARAM = "rfid";
+    public static final String REQ_CHECKTASK = "/checktask";  
+    
+    /**
+     * The constant for commanding and reporting switching the Raspberry Pi state to off.
+     */
+    public static final String COMMAND_SWITCHOFF = "/switchoff";
+    
+    /**
+     * The constant for commanding and reporting switching the Raspberry Pi state to on.
+     */
+    public static final String COMMAND_SWITCHON = "/switchon";
+    
+    /**
+     * The constant for the server to ask for the Raspberry Pi state.
+     */
+    public static final String COMMAND_GETSTATE = "/getstate";
+    
+    /**
+     * The constant for the server to command Raspberry Pi taking a photo.
+     */
+    public static final String COMMAND_TAKEPHOTO = "/takephoto";
+    
+    /**
+     * The constant for the POST request containing a photo.
+     */
+    public static final String UPLOAD_PHOTO = "/upload"; 
+    
+    /**
+     * The constant which is added to the address of a server when reporting motion detection.  
+     */
+    public static final String REQ_MOTIONDETECTED = "/motiondetected";
+    
+    /**
+     * The constant which is added to the address of a server when reporting the taking of a new photo.  
+     */
+    public static final String REQ_PHOTOTAKEN = "/photo";  
+
+    /**
+     * The constant specifies the part to add to the server path to report switching the state of a program by a token.
+     */
+    public static final String REQ_RFIDSWITCH = "/rfid";
+    
+    /**
+     * The definition of the answer when no data are available.
+     */
+    public static final String NO_ANSWER = "no answer";
+    
+    /**
+     * The definition of the answer when everything went as expected.
+     */
+    public static final String OK_ANSWER = "ok";
+    
+    /**
+     * The definition of the answer when error occurred.
+     */
+    public static final String ERROR_ANSWER = "error";
     
     /**
      * The constant for the POST request parameter of time.
@@ -65,24 +102,49 @@ public class SafeHomeServer extends HttpServlet {
     public static final String TIME_PARAM = "time";
     
     /**
+     * The constant for the POST request parameter of this Raspberry Pi ID.
+     */
+    public static final String RPI_PARAM = "rpi";
+    
+    /**
      * The constant for the POST request parameter of photo.
      */
     public static final String PHOTO_PARAM = "photo";
     
     /**
+     * The constant for the POST request parameter containing the name associated with used RFID token.
+     */
+    public static final String RFID_PARAM = "rfid";
+    
+    /**
+     * The constant for the GET request parameter containing short answer.
+     */
+    public static final String ANSWR_PARAM = "answer";
+
+    /**
      * The constant for key of a property for insertion into database.
      */
     public static final String COMMAND_PARAM = "command";
     
+    /**
+     * The path to store incoming pictures in.
+     */
     public static final String PHOTO_PATH = "Pictures\\";
 	
+    /**
+     * The map of tasks for Raspberry Pi to send upon {@link SafeHomeServer#REQ_CHECKTASK}.
+     */
 	public static ConcurrentMap<String, ConcurrentLinkedQueue<String>> forRpi = new ConcurrentHashMap<>();
+	
+	/**
+	 * The map of tasks for the client application to send upon {@link SafeHomeServer#REQ_CHECKTASK}.
+	 */
 	public static ConcurrentMap<String, ConcurrentLinkedQueue<String>> forApp = new ConcurrentHashMap<>();
 
 	private static Logger log = Logger.getLogger("My Servlet");
 	
 	/**
-	 * The method processes GET requests depending on the url-pattern, which caused the reaction of the servlet. 
+	 * The method processes GET requests depending on the URL-pattern, which triggered the servlet. 
 	 * @throws ServletException, IOException.
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
